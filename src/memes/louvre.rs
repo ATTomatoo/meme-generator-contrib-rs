84b454f7cc7882704919b016c4b8991e1bcdd39a
ATTomatoo <1126160939@qq.com>
2025-10-19 01:33:54 +0800

    5

--- src/memes/louvre.rs
+++ src/memes/louvre.rs
@@ -48,8 +48,8 @@ fn make_gradient(width: i32, height: i32) -> Result<Image, Error> {
 }
 
 fn louvre(images: Vec<InputImage>, _texts: Vec<String>, _options: Louvre) -> Result<Vec<u8>, Error> {
-    // 参考其他成功模块的处理方式
-    let base = images.into_iter().next().unwrap().into_raw()?;
+    // 参考其他成功模块的处理方式 - 使用 as_image() 或直接访问字段
+    let base = images.into_iter().next().unwrap().as_image()?;
     let size = base.dimensions();
     let (w, h) = (size.width, size.height);
 
@@ -77,7 +77,7 @@ fn louvre(images: Vec<InputImage>, _texts: Vec<String>, _options: Louvre) -> Res
     // 编码为PNG - 使用项目中其他模块的错误处理方式
     let png_data = result
         .encode(None, EncodedImageFormat::PNG, 100)
-        .ok_or_else(|| Error::from("Failed to encode image"))?;
+        .ok_or("Failed to encode image".into())?;
 
     Ok(png_data.as_bytes().to_vec())
 }
